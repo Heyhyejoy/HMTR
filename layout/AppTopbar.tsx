8071db43cd87658978c846cffc6bd1782e660d31
heyhyejoy <jhyeju1@gmail.com>
2025-08-06 10:05:07 +0300

    Refactor code structure for improved readability and maintainability

--- layout/AppTopbar.tsx
+++ layout/AppTopbar.tsx
@@ -19,7 +19,7 @@ const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
     }));
 
     return (
-        <div className="layout-topbar">
+        <div className="layout-topbar bg-white">
             <Link href="/" className="layout-topbar-logo">
                 <img src={`/layout/images/logo-${layoutConfig.colorScheme !== 'light' ? 'white' : 'dark'}.svg`} width="180px" height={'50px'} alt="logo" />
             </Link>
